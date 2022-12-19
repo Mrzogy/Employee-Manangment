f4cf5b3e7828f515caca39c14508ed49504fdcf9
mrzogy <magbolmrzogy@gmail.com>
2022-12-19 20:11:46 +0300

    Initial Commit

---
+++ Employee Manangment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employee Manangment
+//
+//  Created by 3bood on 25/05/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
